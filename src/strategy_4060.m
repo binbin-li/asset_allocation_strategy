b04cea0482755081fc6db53a6c086738dca61a89
Binbin Li <libinbin050215@gmail.com>
2018-04-14 23:07:23 -0700

    feat: Setup

---
+++ src/strategy_4060.m
@@ -0,0 +1,73 @@
+clear;
+%% 读取数据;
+data=xlsread('../data/data.xls','hs300_5yearbond');
+%计算总天数（样本数）;
+totalDays=size(data,1);
+%时间日期序列不变，数据从2009年1月5日开始，股票和债券指数根据初始值变为1;
+for col=1:3
+	price(:,col) = data(128:totalDays, col);
+end
+
+%% calculate price from 2009/01/05
+price(:,4) = data(128:totalDays,4) / data(128,4);
+price(:,5) = data(128:totalDays,5) / data(128,5);
+days_since_2009_01_05 = size(price,1);
+
+%% 二、构造40/60组合，月度调仓
+%% 2.1调用股票和债券的price数据
+port_4060 = price;
+port_4060 = [port_4060 zeros(days_since_2009_01_05, 1)];
+
+%% 2.2构造初始组合，买了0.4单位的股票和0.6单位的债券，每个月调仓后的20个交易日内，组合净值等于40%股票价格+60%*债券价格
+stock_w = zeros(days_since_2009_01_05,1);%股票数序列
+bond_w = zeros(days_since_2009_01_05,1);%债券数序列
+ 
+%第一个月月初股票数和债券数
+stock_w(1) = 0.4;
+bond_w(1) = 0.6;
+for day = 1:20
+	stock_w(day) = stock_w(1);
+	bond_w(day) = bond_w(1);
+	port_4060(day,6) = stock_w(day) * port_4060(day,4) + bond_w(day) * port_4060(day,5);
+end
+
+%% 2.3循环重复上述步骤
+for k = 1:floor(days_since_2009_01_05/20)
+	start_date = k*20 + 1;
+	%月末调仓时，重新平衡，使得股票和债券的资金比重分别为0.4和0.6，计算二者的购买单位;
+	stock_w(start_date) = 0.4*port_4060(k*20,6)/port_4060(start_date,4);
+	bond_w(start_date) = 0.6*port_4060(k*20,6)/port_4060(start_date,5);
+	%计算每天的组合净值;
+	for day = start_date:min(k*20+20,days_since_2009_01_05)%防止超过样本数;
+		stock_w(day) = stock_w(start_date);
+		bond_w(day) = bond_w(start_date);
+		port_4060(day,6) = stock_w(day)*port_4060(day,4)+bond_w(day)*port_4060(day,5);
+	end
+end
+
+%% 2.4 40/60组合的评价;
+%% 计算组合收益率;
+return_rate = zeros(days_since_2009_01_05,1);
+port = port_4060(:,6);
+for day = 2:days_since_2009_01_05
+   return_rate(day) = port(day)/port(day-1) - 1;
+end
+
+%计算组合的累计收益率、年化收益率、年化波动率%
+accumulative_return_rate = port(days_since_2009_01_05)/port(1) - 1;
+annual_yield = (port(days_since_2009_01_05)/port(1)-1)/days_since_2009_01_05*250;
+annual_volatility = std(return_rate)*sqrt(250);
+%计算组合的最大回撤;
+draw = zeros(days_since_2009_01_05,1);
+for day = 2:days_since_2009_01_05
+	draw(day) = 1 - port(day)/max(port(1:day));
+end
+max_draw = max(draw);
+
+%% 2.5 画股票、债券、40/60组合走势图
+day = 1:days_since_2009_01_05;
+stock = port_4060(:,4);
+bond = port_4060(:,5);
+port4060 = port_4060(:,6);
+plot(day,stock,day,bond,day,port4060);
+
---
+++ src/strategy_parity.m
@@ -0,0 +1,122 @@
+clear;
+%% 一、读取数据;
+data = xlsread('../data/data.xls','hs300_5yearbond');
+%计算总天数（样本数）;
+total_days = size(data,1);
+%从2009年1月5日开始的价格序列;
+price = data(128:total_days,:);
+price(:,4) = price(:,4)/price(1,4);
+price(:,5) = price(:,5)/price(1,5);
+days_since_2009_01_05 = size(price,1);
+
+%% 二、构造基础的风险平价组合，月度调仓
+%% 2.1净值从2009年1月5日开始
+port_parity = zeros(days_since_2009_01_05,6);
+for col = 1:5
+    port_parity(:,col) = price(:,col);
+end
+
+%% 2.2计算股票和债券收益率序列
+
+%% 2008年6月30日开始的收益率序列
+return_since_06_30 = zeros(total_days,5);
+for col = 1:3
+    return_since_06_30(:,col) = data(:,col);
+end
+
+for day = 2:total_days
+   return_since_06_30(day,4) = data(day,4)/data(day-1,4) - 1;%ret第4列为股票收益率序列
+   return_since_06_30(day,5) = data(day,5)/data(day-1,5) - 1;%ret第5列为债券收益率序列
+end
+
+%% 2.3估计股债的相关系数和波动率，确定二者权重
+
+%生成波动率、相关系数、股票债券资金权重序列;
+coe = zeros(days_since_2009_01_05,1);
+stock_vol = zeros(days_since_2009_01_05,1);
+bond_vol = zeros(days_since_2009_01_05,1);
+stock_weight = zeros(days_since_2009_01_05,1);
+bond_weight = zeros(days_since_2009_01_05,1);
+ 
+%计算股票和债券收益率序列的相关系数（2009年1月5日的前120个交易日的数据）;
+coe(1) = min(min(corrcoef(return_since_06_30(8:127,4),return_since_06_30(8:127,5))));
+
+%简单移动平均法预测波动率;
+stock_vol(1) = std(return_since_06_30(8:127,4))*sqrt(250);
+bond_vol(1) = std(return_since_06_30(8:127,5))*sqrt(250);
+
+stock_w = zeros(days_since_2009_01_05,1);
+bond_w = zeros(days_since_2009_01_05,1);
+stock_weight(1) = (1/stock_vol(1)) / (1/stock_vol(1)+1/bond_vol(1));%计算权重
+bond_weight(1) = 1 - stock_weight(1);
+
+stock_w(1) = stock_weight(1) / port_parity(1,4);%计算股票数;
+bond_w(1) = bond_weight(1) / port_parity(1,5);%计算债券数;
+
+%计算风险平价策略净值;
+for day = 1:20
+    coe(day) = coe(1);
+    stock_weight(day) = stock_weight(1);
+    bond_weight(day) = bond_weight(1);
+    stock_w(day) = stock_w(1);
+    bond_w(day) = bond_w(1);
+    port_parity(day,6) = stock_w(day)*port_parity(day,4) + bond_w(day)*port_parity(day,5);
+end
+
+%% 2.4 循环重复上述步骤
+for k = 1:floor(days_since_2009_01_05/20)
+    start_date = 1 + 20*k;
+    %计算调仓日前120个交易日的股票和债券收益率序列的相关系数;
+    coe(start_date) = min(min(corrcoef(return_since_06_30(8+20*k:127+20*k,4),return_since_06_30(8+20*k:127+20*k,5))));
+
+    %简单移动平均法预测波动率;
+    stock_vol(start_date) = std(return_since_06_30(8+20*k:127+20*k,4))*sqrt(250);
+    bond_vol(start_date) = std(return_since_06_30(8+20*k:127+20*k,5))*sqrt(250);
+
+    %根据风险平价公式计算股票和债券的资金权重;
+    stock_weight(start_date) = (1/stock_vol(start_date))/(1/stock_vol(start_date) + 1/bond_vol(start_date));
+    bond_weight(start_date) = 1 - stock_weight(start_date);
+
+    %月末调仓时，重新平衡，使得股票和债券的资金比重分别为stock_weight和bond_weight，计算二者的购买单位;
+    stock_w(start_date) = stock_weight(start_date)*port_parity(20*k,6)/port_parity(start_date,4);
+    bond_w(start_date) = bond_weight(start_date)*port_parity(20*k,6)/port_parity(start_date,5);
+
+    %计算风险平价策略净值;
+    for day = start_date:min(20+20*k,days_since_2009_01_05)
+        coe(day) = coe(start_date);
+        stock_weight(day) = stock_weight(start_date);
+        bond_weight(day) = bond_weight(start_date);
+        stock_w(day) = stock_w(start_date);
+        bond_w(day) = bond_w(start_date);
+        port_parity(day,6) = stock_w(day)*port_parity(day,4) + bond_w(day)*port_parity(day,5);
+    end
+end
+   
+%% 三、基础风险平价组合的评价
+%% 3.1计算组合收益率
+return_port = zeros(days_since_2009_01_05,1);
+port = port_parity(:,6);
+for day = 2:days_since_2009_01_05
+   return_port(day) = port(day)/port(day-1) - 1;
+end
+
+%% 3.2计算组合的累计收益率、年化收益率、年化波动率%
+accumulative_return = port(days_since_2009_01_05)/port(1)-1;
+annual_return = (port(days_since_2009_01_05)/port(1)-1)/days_since_2009_01_05*250;
+volatility = std(return_port)*sqrt(250);
+
+%% 3.3计算组合的最大回撤
+draw = zeros(days_since_2009_01_05,1);
+for day = 2:days_since_2009_01_05
+    draw(day) = 1 - port(day)/max(port(1:day));
+end
+maxdraw = max(draw);
+
+%% 3.4 画股票、债券、基础风险平价组合走势图
+day = 1:days_since_2009_01_05;
+stock = port_parity(:,4);
+bond = port_parity(:,5);
+parity = port_parity(:,6);
+plot(day,stock,day,bond,day,parity);
+
+
